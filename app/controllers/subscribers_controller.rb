15de407d72687c5e38fb8e274c744d933023ee14
rilian <dmitriyis@gmail.com>
2015-04-26 23:47:36 -0400

    subscriber views

--- app/controllers/subscribers_controller.rb
+++ app/controllers/subscribers_controller.rb
@@ -2,4 +2,39 @@ class SubscribersController < ApplicationController
   def index
     @subscribers = Subscriber.all
   end
+
+  def new
+  end
+
+  def create
+    @subscriber = Subscriber.new(create_subscriber_params)
+    if @subscriber.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  def manage
+    @subscriber = Subscriber.find_by_uuid(params[:uuid])
+  end
+
+  def update
+    @subscriber = Subscriber.find_by_uuid(params[:uuid])
+    if @subscriber.update(update_subscriber_params)
+      redirect_to root_path
+    else
+      render :manage
+    end
+  end
+
+private
+
+  def create_subscriber_params
+    params.require(:subscriber).permit(:email)
+  end
+
+  def update_subscriber_params
+    params.require(:subscriber).permit(:is_active)
+  end
 end
