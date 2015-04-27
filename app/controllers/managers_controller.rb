a35549e93b3ee2a5246ed38d6c64943f59334d0c
rilian <dmitriyis@gmail.com>
2015-04-27 00:31:17 -0400

    managers index view

---
+++ app/controllers/managers_controller.rb
@@ -0,0 +1,45 @@
+class ManagersController < ApplicationController
+  def index
+    @managers = Manager.all
+  end
+
+  def new
+  end
+
+  def create
+    @manager = Manager.new(create_manager_params)
+    if @manager.save
+      redirect_to managers_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    @manager = Manager.find(params[:id])
+    if @manager.update(update_manager_params)
+      redirect_to managers_path
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @manager = Manager.find(params[:id])
+    @manager.destroy
+    redirect_to managers_path
+  end
+
+private
+
+  def create_manager_params
+    params.require(:manager).permit(:email)
+  end
+
+  def update_manager_params
+    params.require(:manager).permit(:email)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root 'home#index'
 
+  resources :managers
   resources :subscribers, only: %i[index new create manage update] do
     get :manage, on: :collection
   end
