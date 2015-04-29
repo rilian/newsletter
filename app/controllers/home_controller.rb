1df24845f7c14908ac80d7f617d67693c953705b
rilian <dmitriyis@gmail.com>
2015-04-28 21:19:51 -0400

    update home navigation

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,6 @@
 class HomeController < ApplicationController
   def index
     @subscriber = Subscriber.new
+    @issues = Issue.all.order(id: :desc)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
 
   resources :managers
   resources :subscribers, only: %i[index new create manage update] do
+  resources :managers, except: %i[show]
+  resources :subscribers, except: %i[show edit destroy] do
     get :manage, on: :collection
   end
 end
