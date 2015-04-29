4ad32dbe4e789b291826a171a8400171952bbcd7
rilian <dmitriyis@gmail.com>
2015-04-28 21:20:01 -0400

    issues routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
   root 'home#index'
 
-  resources :managers
-  resources :subscribers, only: %i[index new create manage update] do
+  resources :issues
   resources :managers, except: %i[show]
   resources :subscribers, except: %i[show edit destroy] do
     get :manage, on: :collection
