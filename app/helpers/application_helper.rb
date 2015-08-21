4b13d076b93a073aca166e33c1041e93683f9431
sudodoki <smd.deluzion@gmail.com>
2015-08-21 09:36:14 +0300

    Add sidebar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,17 @@ module ApplicationHelper
       when :alert then 'flash-warn'
     end
   end
+
+  def sidemenu_link_to(text, path, options = {})
+    ccs_class = "menu-item"
+    ccs_class += " selected" if current_page?(path)
+    options.merge!(class: ccs_class)
+    if icon = options[:icon]
+      link_to(path, options) do
+        "<span class='octicon octicon-#{icon}'></span>#{text}".html_safe
+      end
+    else
+      link_to(text, path, options)
+    end
+  end
 end
