1e67859d1c20786c7ecb3c88627c4698363b7f69
rilian <dmitriyis@gmail.com>
2016-01-11 16:22:21 +0200

    upd gems

--- config/initializers/errbit.rb
+++ config/initializers/errbit.rb
@@ -1,6 +1,7 @@
 Airbrake.configure do |config|
-  config.api_key = ENV['ERRBIT_API_KEY']
+  config.environment = Rails.env
+  config.ignore_environments = %w[test]
   config.host = ENV['ERRBIT_HOST']
-  config.port = 80
-  config.secure = config.port == 443
+  config.project_id = true
+  config.project_key = ENV['ERRBIT_API_KEY']
 end
