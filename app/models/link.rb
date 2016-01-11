7f92a0ab0b7067f8f3c56e3e3691f15b49720f16
rilian <dmitriyis@gmail.com>
2016-01-11 16:22:16 +0200

    validate uniqueness of Link url

--- app/models/link.rb
+++ app/models/link.rb
@@ -2,7 +2,7 @@ class Link < ActiveRecord::Base
   belongs_to :issue
   belongs_to :manager
 
-  validates :url, presence: true
+  validates :url, presence: true, uniqueness: true
 
   scope :without_issue, ->() { where(issue_id: nil) }
 
