6d486d859afae59ee35d788266af5256e8579104
Ihor Kroosh <ihor.kroosh@gmail.com>
2016-01-20 01:54:45 +0200

    added links cleanup

--- app/models/link.rb
+++ app/models/link.rb
@@ -7,4 +7,12 @@ class Link < ActiveRecord::Base
   scope :without_issue, ->() { where(issue_id: nil) }
 
   delegate :title, to: :issue, prefix: true, allow_nil: true
+
+  before_save :cleanup_link
+
+  TEXT_TO_OMIT = ['?utm_source=rubyweekly&utm_medium=email']
+
+  def cleanup_link
+    self.url = TEXT_TO_OMIT.reduce(url) { |url, to_omit| url.gsub(to_omit, '') }
+  end
 end
