a7bdf6c3593a38a362ef9ebfcb0d4ac7cb54a522
Ihor Kroosh <ihor.kroosh@gmail.com>
2016-01-20 15:51:26 +0200

    fixed links cleanup to clean it by get params

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,3 +1,5 @@
+require 'uri'
+
 class Link < ActiveRecord::Base
   belongs_to :issue
   belongs_to :manager
@@ -10,9 +12,16 @@ class Link < ActiveRecord::Base
 
   before_save :cleanup_link
 
-  TEXT_TO_OMIT = ['?utm_source=rubyweekly&utm_medium=email']
+  PARAMS_TO_OMIT = %w[utm_source utm_medium]
 
   def cleanup_link
-    self.url = TEXT_TO_OMIT.reduce(url) { |url, to_omit| url.gsub(to_omit, '') }
+    self.url = URI(url).tap do |parsed_url|
+      parsed_url.query = parsed_url.query
+        .split('&').lazy
+        .map { |param| param.split('=') }
+        .reject { |param| PARAMS_TO_OMIT.include? param.first }
+        .map { |param| param.join('=') }
+        .to_a.join('&')
+    end.to_s
   end
 end
