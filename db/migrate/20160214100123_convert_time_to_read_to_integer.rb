1f1a4e7ffa17c15601b2c1ccc6505ad3a3d5fe31
Ihor Kroosh <ihor.kroosh@gmail.com>
2016-02-18 12:57:05 +0200

    fixes in order to comments

--- db/migrate/20160214100123_convert_time_to_read_to_integer.rb
+++ db/migrate/20160214100123_convert_time_to_read_to_integer.rb
@@ -1,5 +1,6 @@
 class ConvertTimeToReadToInteger < ActiveRecord::Migration
   def change
-    change_column :links, :time_to_read, "integer USING CAST(REGEXP_REPLACE(COALESCE(time_to_read,'0'), '[^0-9]+', '', 'g') AS INTEGER)"
+    remove_column :links, :time_to_read
+    add_column :links, :time_to_read, :integer
   end
 end
