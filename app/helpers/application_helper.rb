c80ddebca49a9f65bc5c08601f04391634d03640
pavel-zhuravlyov <pavlozhuravlyov@gmail.com>
2016-10-13 15:08:55 +0300

    fix application helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,20 +1,16 @@
 # frozen_string_literal: true
 module ApplicationHelper
   def flash_class(level)
-    case level
-    when :notice then ''
-    when :error then 'flash-error'
-    when :alert then 'flash-warn'
-    end
+    { notice: '', error: 'flash-error', alert: 'flash-warn' }[level]
   end
 
   def sidemenu_link_to(text, path, options = {})
     css_class = 'menu-item'
     css_class += ' selected' if current_page?(path)
     options.merge!(class: css_class)
-    if (icon = options[:icon]).present?
+    if options[:icon].present?
       link_to(path, options) do
-        "<span class='octicon octicon-#{icon}'></span>#{text}".html_safe
+        "<span class='octicon octicon-#{options[:icon]}'></span>#{text}".html_safe
       end
     else
       link_to(text, path, options)
