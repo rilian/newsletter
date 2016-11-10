bc1db9e96afbe5fa4db72cc1c0f5e84609e190eb
rilian <dmitriyis@gmail.com>
2016-11-10 14:58:19 +0200

    сщтые

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,5 +1,11 @@
 # frozen_string_literal: true
 Rails.application.configure do
+  MAIL_DEFAULTS = {
+    port: 587,
+    authentication: :plain,
+    enable_starttls_auto: true
+  }
+
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
