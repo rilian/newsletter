325bc1943171cf5cb7c3c099c436a6c3a5ffd1f8
Pavel Zhuravlyov <pavlozhuravlyov@gmail.com>
2016-11-17 12:30:51 +0200

    add search to root page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,5 +3,6 @@ class HomeController < ApplicationController
   def index
     @subscriber = Subscriber.new
     @issues = Issue.page(params[:page]).all.order(id: :desc)
+    @q = Link.ransack(params[:q])
   end
 end
